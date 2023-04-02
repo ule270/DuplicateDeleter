a10cbc86d559765fc9a88cb2d57ac89fb3795cf1
ule270 <u.le270@gmail.com>
2023-04-02 12:52:09 -0400

    all integer tests passing

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -36,6 +36,23 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 //    removes all values in the array which occur exactly the specified number of times.
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new Integer[0];
+        StringBuilder noDupes = new StringBuilder();
+//        counting number of occurrences
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j])
+                    count++;
+            }
+//            if count not equal to int number of times occurs
+            if (!(count == exactNumberOfDuplications)) {
+                noDupes.append(array[i]);
+            }
+        }
+        Integer[] removed = new Integer[noDupes.length()];
+        for (int i = 0; i < noDupes.length(); i++) {
+            removed[i] = Integer.parseInt("" + noDupes.charAt(i));
+        }
+        return removed;
     }
 }
