1abd366ce6b3490f5368e2dd33f22a4f66b73b12
ule270 <u.le270@gmail.com>
2023-04-02 18:30:47 -0400

    string duplicates completed.

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -11,11 +11,51 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
     }
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        StringBuilder noDupes = new StringBuilder();
+//        counting number of occurrences
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j])
+                    count++;
+            }
+//            if count not equal to or not greater than int number of times occurs
+            if (!(count >= maxNumberOfDuplications)) {
+                noDupes.append(array[i]+ " ");
+            }
+        }
+//        if no matches, then return new array of length 0.
+        if (noDupes.length() == 0) {
+            return new String[0];
+        }else {
+//            otherwise split the String with " "
+//            .split() method returns an array of strings
+            return noDupes.toString().split(" ");
+        }
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        StringBuilder noDupes = new StringBuilder();
+//        counting number of occurrences
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j])
+                    count++;
+            }
+//            if count not equal to or not greater than int number of times occurs
+            if (!(count == exactNumberOfDuplications)) {
+                noDupes.append(array[i]+ " ");
+            }
+        }
+//        if no matches, then return new array of length 0.
+        if (noDupes.length() == 0) {
+            return new String[0];
+        }else {
+//            otherwise split the String with " "
+//            .split() method returns an array of strings
+            return noDupes.toString().split(" ");
+        }
     }
 }
